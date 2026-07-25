657d63e22b137b139c44010129219e507fb9a266
Vishnu Prakash <vishnu07jassi@gmail.com>
2026-07-26 01:44:05 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if (intervals.empty())
+            return 0;
+        
+        int n = intervals.size();
+
+        sort(intervals.begin(),intervals.end(),[](Interval &a,Interval &b){
+            return a.start < b.start;
+        });
+        
+        priority_queue<int,vector<int>,greater<int>> ans;
+        ans.push(intervals[0].end);
+        for (int i=1;i<n;i++){
+            if (intervals[i].start >= ans.top()){
+                ans.pop();
+                ans.push(intervals[i].end);
+            }
+            else
+                ans.push(intervals[i].end);
+        }
+        return ans.size();
+    }
+};
