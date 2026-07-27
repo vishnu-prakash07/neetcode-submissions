bdf229b0a0479a3fc8c086062416005e6db08135
Vishnu Prakash <vishnu07jassi@gmail.com>
2026-07-27 11:40:59 +0530

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    vector<double> getTime(int n,const vector<vector<int>>& info,int target){
+        //info = {pos,speed,i}
+        vector<double> time;
+
+        for (int i=0;i<n;i++){
+            int distance = target - info[i][0];
+            double timeCal = (double)distance / info[i][1];
+            time.push_back(timeCal);
+        }
+        return time;
+    }
+
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        vector<vector<int>> info(n);
+        for (int i=0;i<n;i++){
+            info[i]= {position[i],speed[i],i};
+        }
+        
+        sort(info.begin(),info.end(),[](vector<int> &a,vector<int> &b){
+            return a[0] > b[0];
+        });
+
+        vector<double> time = getTime(n,info,target);
+
+        double timeMax = 0.0;
+        int count = 0;
+
+        for (int i=0;i<n;i++){
+            if (time[i] > timeMax){
+                timeMax = time[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
