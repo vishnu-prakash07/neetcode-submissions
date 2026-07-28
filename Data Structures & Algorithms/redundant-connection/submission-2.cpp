c7e047fe8704589d22ea5a1fc340d6eb16c81dec
Vishnu Prakash <vishnu07jassi@gmail.com>
2026-07-28 19:43:32 +0530

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    int findParent(int node,vector<int>& parent){
+        if (node == parent[node])
+            return node;
+        return parent[node] = findParent(parent[node],parent);
+    }
+
+    void unionSet(int u,int v,vector<int>& parent,vector<int>& size){
+        int pu = findParent(u,parent);
+        int pv = findParent(v,parent);
+
+        if (pu == pv)
+            return;
+        
+        if (size[pu] < size[pv]){
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else{
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> parent(n+1);
+        vector<int> size(n+1,1);
+
+        for (int i=1;i<=n;i++)
+            parent[i] = i;
+
+        for (auto &edge : edges){
+            if (findParent(edge[0],parent) == findParent(edge[1],parent))
+                return edge;
+            else
+                unionSet(edge[0],edge[1],parent,size);
+        }
+        return {};
+    }
+};
